5734494ea2c973755e23738f2fb3f7ce6a51bb6a
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-08-22 10:34:09 +0330

    added blog section
    created an app named 'blogapp' because the word 'blog' is reserved by python. fixed the blog pages and now works fine. also edited 'base.html' so it can redirect the user to blog pages.

---
+++ blogapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from blogapp.views import *
+
+app_name = 'blogapp'
+
+urlpatterns = [
+    path('', blog_view, name = 'index'),
+    path('single', blog_single, name = 'single'),
+]
