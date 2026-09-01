bb86341bedb4544536de5942dc17d6442f50a8f5
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-01 21:12:55 +0330

    learned how to import data from database
    1. for blogapp, made a new url, view, and template called 'test'
    2. in view, imported the class 'Post' and read data from database and gave it to 'test'
    3. used jinja to display 'posts' in web page

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from blogapp.models import Post
 
 def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
+
+def test(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request, 'test.html', context)
