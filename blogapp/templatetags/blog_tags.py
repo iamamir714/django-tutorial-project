b0bcd00804e73fbc26b903fd091b2dd398b6724c
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-08 00:24:06 +0330

    made an inclusion template tag
    for finding the most recent posts

--- blogapp/templatetags/blog_tags.py
+++ blogapp/templatetags/blog_tags.py
@@ -15,3 +15,8 @@ def function():
 @register.filter
 def snippet(value, arg=20):
     return value[:arg] + '...'
+
+@register.inclusion_tag('popularposts.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
+    return {'posts':posts}
