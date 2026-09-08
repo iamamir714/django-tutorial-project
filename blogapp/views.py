f89fc9e1f013b51950e233ea6d6d1144b70de296
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-08 10:26:10 +0330

    improved previous code
    now i can select categories from the side bar 'Post Categories'

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from blogapp.models import Post
 
-def blog_view(request):
+def blog_view(request, cat_name=None):
     posts = Post.objects.filter(status=1)
+    if cat_name:
+        posts = posts.filter(category__name=cat_name)
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
