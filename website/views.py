5a198448338036044b7623a51a6d91e101351b7c
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-09 18:43:30 +0330

    made a simple form
    now i can get data from contacts and save them in database

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from website.models import Contact
 
 def index_view(request):
     return render(request, 'website/index.html')
@@ -9,3 +10,20 @@ def about_view(request):
 
 def contact_view(request):
     return render(request, 'website/contact.html')
+
+def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        c = Contact()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.message = message
+        c.save()
+        print(name, email, subject, message)
+
+    return render(request, 'test.html', {})
