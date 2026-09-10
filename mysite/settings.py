9e34e82df3e65b1a9c7dfa2e2cb2f4d619f67538
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-10 20:39:14 +0330

    made a sitemap.xml
    sitemap is something that bots are going to use to get the urls.
    made a sitemap for each of my apps.
    a static sitemap for 'website'
    and a dynamic sitemap for 'blog'

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,9 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'django_extensions',
     'website.apps.WebsiteConfig',
     'blogapp'
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,11 +18,20 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from website.sitemaps import StaticViewSitemap
+from blogapp.sitemaps import BlogSitemap
+
+sitemaps = {
+    "static": StaticViewSitemap,
+    'blogapp': BlogSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
-    path('blog/', include('blogapp.urls'))
+    path('blog/', include('blogapp.urls')),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap",)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
