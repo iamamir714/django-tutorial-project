f00cf139e66cf06aa74167b757665ae0c1fa4a38
Amir Hossein Halimi <i.am.amir.714@gmail.com>
2026-09-17 21:13:06 +0330

    added robots.txt
    its a page that tells which user has access to which page. for example normal users should not have access to '/admin' page

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,12 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'robots',
     'django_extensions',
     'website.apps.WebsiteConfig',
     'blogapp'
 ]
 
+# sites framework
 SITE_ID = 2
+
+# robots
+ROBOTS_USE_HOST = False
+ROBOTS_USE_SITEMAP = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
